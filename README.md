a179490b126567d717adbc56782f45076edb3cbd
Pano Papadatos <pano@heypano.com>
2022-06-15 17:46:16 -0400

    working auto import

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Resources
 - https://dev.to/alexeagleson/how-to-create-and-publish-a-react-component-library-2oe
 - https://javascript.plainenglish.io/react-component-library-with-ts-and-rollup-77aca0c727bf
+- https://javascript.plainenglish.io/how-to-create-your-own-react-component-library-e305a1a53b49
--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,24 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
+import typescript from "rollup-plugin-typescript2";
+import postcss from "rollup-plugin-postcss";
+import { babel } from '@rollup/plugin-babel';
 
-const packageJson = require("./package.json");
-
-export default [
-    {
-        input: "src/index.ts",
-        output: [
-            {
-                file: packageJson.main,
-                format: "cjs",
-                sourcemap: true,
-            },
-            {
-                file: packageJson.module,
-                format: "esm",
-                sourcemap: true,
-            },
-        ],
-        plugins: [
-            resolve(),
-            commonjs(),
-            typescript({ tsconfig: "./tsconfig.json" }),
-        ],
-    },
-    {
-        input: "dist/esm/types/index.d.ts",
-        output: [{ file: "dist/index.d.ts", format: "esm" }],
-        plugins: [dts()],
-    },
-];
+export default {
+    input: "./src/index.ts",
+    output: [
+        {
+            file: "dist/index.js",
+            format: 'es'
+        }
+    ],
+    plugins: [
+        peerDepsExternal(),
+        resolve(),
+        typescript(),
+        postcss({
+            extensions: ['.css']
+        }),
+        babel()
+    ]
+}
