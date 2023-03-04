36fdd85e2ef8d25f2c3e31a711ea3905aa933c25
Pano Papadatos <pano@heypano.com>
2023-03-04 14:24:21 -0500

    feat(cat): wip

---
+++ src/components/DrawWithin/DrawWithin.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { CatPath } from "./cat/paths";
+
+interface DrawWithinProps {}
+
+function DrawWithin(props: DrawWithinProps) {
+  return (
+    <svg viewBox="0 0 202.53 230.74">
+      <CatPath />
+    </svg>
+  );
+}
+export default DrawWithin;
