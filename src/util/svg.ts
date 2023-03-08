c87e2e49a7040ede78291f7f59a90e726cb4e632
Pano Papadatos <pano@heypano.com>
2023-03-08 16:51:29 -0500

    fix: simplify getPathFromPoints

--- src/util/svg.ts
+++ src/util/svg.ts
@@ -28,20 +28,12 @@ function getPointInSvg(svg: SVGSVGElement, otherX: number, otherY: number) {
 }
 
 export const getPathFromPoints = (points: Array<Point>) => {
-  return points.reduce((path, point, index) => {
+  return points.reduce((path, point) => {
     const { x, y, type } = point;
-    const isNotFirstPoint = index > 0;
-    const previousPoint = isNotFirstPoint
-      ? points[index - 1]
-      : { x: null, y: null, type: null };
-    const { x: x0, y: y0 } = previousPoint;
     const needToOpen = type === "M";
-    const isSameAsLast = x0 === x && y0 === y;
 
     if (needToOpen) {
       return `${path} M ${x} ${y}`;
-    } else if (isSameAsLast) {
-      return `${path} L ${x + 5} ${y0 + 5}`;
     } else {
       return `${path} L ${x} ${y}`;
     }
